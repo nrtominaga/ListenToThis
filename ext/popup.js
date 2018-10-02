749b16d1b4483fd6a6eaef6d9096c8bbc00c8c35
Naoki Tominaga <u0876779@utah.edu>
2018-10-01 19:41:02 -0600

    still working on capturing media stream

--- ext/popup.js
+++ ext/popup.js
@@ -1,15 +1,11 @@
 let record = document.getElementById('record');
 
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
+record.onclick = function() {
+    chrome.tabCapture.capture({
+        video:false,
+        audio:true
+    },
+    function(stream){
 
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
 };
